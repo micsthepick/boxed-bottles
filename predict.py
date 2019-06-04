8b0bc7d462c5b0afd73150bf3b04c159ee139b8f
Andrew Thompson <mrwerdo331@me.com>
2019-06-04 16:07:21 +1000

    Added driver

--- predict.py
+++ predict.py
@@ -2,18 +2,54 @@
 import turicreate as tc
 import sys
 
-if len(sys.argv) != 3:
-    print('error')
-    sys.exit(-1)
+def warn(*args, **kwargs):
+    pass
 
-model = tc.load_model(sys.argv[2])
+import warnings
+warnings.warn = warn
 
-data = tc.SFrame(sys.argv[1])
+def predict_model_A(img_paths):
+    print('turicreate')
+    model = tc.load_model('models/512x384v4.model')
 
-train_data, test_data = data.random_split(0.8, seed=7)
+    input_paths = tc.SArray(img_paths[0])
+    images = tc.SArray([tc.Image(path) for path in img_paths[0]])
+    output_paths = tc.SArray(img_paths[1])
 
-predictions = model.predict(test_data)
+    data = tc.SFrame({
+        'input': input_paths,
+        'image': images,
+        'output': output_paths
+        })
 
-test = tc.SFrame({'image': test_data["image"], 'predictions': predictions})
-test['image_with_predictions'] = tc.object_detector.util.draw_bounding_boxes(test['image'], test['predictions'])
-test[['image', 'image_with_predictions']].explore()
+    predictions = model.predict(data)
+
+    ddata = tc.SFrame({
+        'input': input_paths,
+        'image': images,
+        'predictions': predictions,
+        'output': output_paths
+        })
+
+    ddata['label'] = tc.object_detector.util.draw_bounding_boxes(ddata['image'], ddata['predictions'])
+
+    for record in ddata:
+        record['label'].save(record['output'])
+
+def predict_model_B(input_img, output_img):
+    print('tensorflow')
+
+def main(model_path, data_path):
+    model = tc.load_model(model_path)
+    data = tc.SFrame(data_path)
+    train_data, test_data = data.random_split(0.8, seed=7)
+    predictions = model.predict(test_data)
+    test = tc.SFrame({'image': test_data["image"], 'predictions': predictions})
+    test['image_with_predictions'] = tc.object_detector.util.draw_bounding_boxes(test['image'], test['predictions'])
+    test.explore()
+
+if __name__=='__main__':
+    if len(sys.argv) != 3:
+        print('error')
+        sys.exit(-1)
+    main(sys.argv[2], sys.argv[1])
